8c3b8e37c66124fd9fe862bcbbdcdadcdf662096
Oksana-dnp <71383222+Oksana-dnp@users.noreply.github.com>
2023-01-24 11:13:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,95 +1,114 @@
-//classNames
+//anagramma
 
+let a = 'hello';
+let b = 'oLeh';
 
-let classNamesList = ['header', 'block', 'task', 'block', 'text', 'link', 'task', 'link', 'task', 'link' ];
+function checkAnagram(str1,str2){
+    
+    return [...(str1.toLowerCase())].sort().toString() === [...(str2.toLowerCase())].sort().toString();
 
-
-
-let classNamesCount = {};
-
-
-for(let item of classNamesList){
-
-    if(classNamesCount[item]){
-        classNamesCount[item]+=1;
-    }else {
-        classNamesCount[item] = 1;
-  
-    }
 }
+console.log(checkAnagram(a,b));
 
 
-let result = Object.keys(classNamesCount).sort((a,b)=>{
-   return classNamesCount[b] - classNamesCount[a]
-}
-)
-
-
-//simple Numbers
+//IsPrime 
 
 function getPrime(num) {
+    //create two arrays , one  for not prime numbers, one for prime numbers
     let seive = [];
     let primes = [];
-
-    for(let i=2; i <= num; i++){
-        console.log('i-->', seive[i])
-        if(!seive[i]) {
+    //create the loop
+    for (let i = 2; i <= num; i++) {
+        if (!seive[i]) {
             primes.push(i);
-            for(let j = i*2; j <= num; j+=i){
+            //ми маємо виключити всі цифри, які діляться на i, наприклад при першій ітерації, всі, що діляться на 2 і додати їх в масив "сито"
+            for (let j = i * 2; j <= num; j += i) {
                 seive[j] = true;
             }
         }
     }
-    console.log('seive-->', typeof(seive))
-
-    return primes
+    return primes;
 }
 
-console.log(getPrime(160))
 
-function isPrime(num) {
-    let result = [];
-    test: for(; num > 1; num--){
-        for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
-            if (num % i === 0) {
-                continue test;
-            }
-        }
-        result.push(num)
-    }
-    return result
-}
-
-console.log(isPrime(49))
+console.log('getPrime(11) ', getPrime(800))
 
 
-//crossing two arrays
+//merge intervals
 
-let input1 = [4, 9, 4, 5];
-let input2 = [9, 4, 9, 8, 4];
+let arr = [[1,3], [15,18],[2,6], [7,10]]
+console.log(arr[0].length-1)
+//[[1,6],[8,10],[15,18]]
+let interval2 = [[1,4], [4,5]];
 
-function findCrossArr(nums1, nums2){
-    let result = [];
+function merge(intervals){
 
-    let map = nums1.reduce((accum, item)=>{
-        accum[item] = accum[item] ? accum[item] + 1 : 1;
-        return accum
-    }, {})
-    console.log('map',map)
-    for(let i=0; i < nums2.length; i++){
-        let current = nums2[i];
-        console.log('current-->', current);
-        
-        let count = map[current];
-        console.log('count-->', map[current])
-        if(count && count > 0){
-            result.push(current);
-            map[current] -=1;
+    if(intervals.length < 2) return intervals;
+
+    intervals.sort((a,b) => a[0] - b[0]);
+
+    let result = [intervals[0]];
+    for(let interval of intervals){
+        //recent равно последнему интервалу в міссиве result
+        //массивы копируются по сслыке
+        let recent = result[result.length - 1];
+ 
+        if(recent[1] >= interval[0]){
+          /*   console.log('recent[1]', recent[1]);
+            console.log('interval[0]-->',interval[0]) */
+            //проверям оставить последний єлемент в recent  текущий или заменить на крайний следующего интервала
+            //массив скопирован по ссылке, поэтому меняя  recent[1], автоматически меняем крайний интервал в  result[result.length - 1];)
+            recent[1] = Math.max(recent[1], interval[1]);
+        }else{
+            result.push(interval)
         }
 
-    }
-
-    return result;
+    } 
+        return result
 }
-console.log('same__>', findCrossArr(input1, input2));
+
+console.log('merge-->',merge(arr))
+
+//brackets
+
+function isValid(str) {
+    let stack = [];
+    let brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+ 
+    for (let i = 0; i < str.length; i++) {
+        const current = str[i];
+        if (isClosingBrackets(current)) { 
+            if(brackets[current] !== stack.pop())   return false         
+
+        }
+    else {
+        stack.push(current)
+    }
+}
+return  stack.length === 0;
+    
+}
+
+function isClosingBrackets(char) {
+    return [')', '}', ']'].indexOf(char) > -1;
+}
+
+
+let brackets =  '])'
+
+console.log('res-->',isValid(brackets))
+
+let row1 = [1, 2, 2, 1];
+let row2 = [3, 1 ,2];
+
+function crossLineCount(arr, arr2) {
+    let a = arr.map((item, index, array)=>{
+        return item[index] + item[]
+    })
+    
+}
 
