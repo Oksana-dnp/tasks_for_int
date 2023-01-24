143aadc273f5009d98f51213f082eaa96e96521d
Oksana-dnp <ksana.kharchenko@gmail.com>
2023-01-24 12:08:22 +0200

    wall_ready

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//let wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]];
+/* 
 
 let bracketsList = ['{', '[', ']', '}'];
 
@@ -28,4 +28,26 @@ function isClosingBrackets(char) {
     return ['}',']', ')'].indexOf(char) > -1;    
 }
 
-console.log('listValidBrackets-->', listValidBrackets(bracketsList));
+console.log('listValidBrackets-->', listValidBrackets(bracketsList)); */
+
+let wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]];
+
+const leastBrickes = function(wall){
+    let map = {};
+    let max = 0;
+
+   wall.forEach(row => {
+    let sum=0;
+
+    for(let i=0; i < row.length -1; i++){
+        sum += row[i];
+       map[sum] = map[sum] ? map[sum] +1 : 1;
+        max = Math.max(max, map[sum]);
+    } 
+   
+   });
+    return wall.length - max
+}
+
+console.log('obj-->', leastBrickes(wall))
+
